ba3f9a4a640a20f02c7af98e063683e7c223b616
admin <admin@FANTASY>
2020-03-02 18:45:21 +0530

    Changes

---
+++ CMS/CMS.Domain/Models/School.cs
@@ -0,0 +1,21 @@
+﻿using CMS.Domain.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Domain.Models
+{
+    public class School : AuditableEntity
+    {
+        public int SchoolId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [MaxLength(50)]
+        public string CenterNumber { get; set; }
+
+        public int ClientId { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
---
+++ CMS/CMS.Storage/Projections/SchoolProjection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Domain.Storage.Projections
+{
+    public class SchoolProjection
+    {
+        public int SchoolId { get; set; }
+
+        public string Name { get; set; }
+
+        public string CenterNumber { get; set; }
+
+        public int ClientId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string ClientName { get; set; }
+
+        public string CurrentUserRole { get; set; }
+
+    }
+}
